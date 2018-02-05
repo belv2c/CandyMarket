a338813f94d7bfc76b9e2ca9d7d2572fcdcc24de
Brooke Lewis <belv2c@gmail.com>
2018-02-05 14:15:28 -0600

    added users enum

---
+++ CandyMarket/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CandyMarket
+{
+    enum Users
+    {
+        Andy = 1,
+        Sports,
+        Jeff,
+        Jess,
+        Casey,
+        Hayley
+    }
+}
